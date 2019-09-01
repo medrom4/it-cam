223278795214b0f6ce2ed82e0ab1208648b26e6e
medrom4 <medikroma@gmail.com>
2019-09-01 21:14:03 +0300

    readme

--- src/App.js
+++ src/App.js
@@ -3,11 +3,30 @@ import logo from './logo.svg';
 import './App.css';
 
 const App = () => {
+  return (
+    <div>
+      <Header />
+      <Technologies />
+    </div>    
+  );
+}
+
+const Header = () => {
+  return ( 
+    <div>
+      <a href="#s">Home</a>
+      <a href="#s">News</a>
+      <a href="#s">Contact</a>
+    </div>
+  );
+}
+
+const Technologies = () => {
   return ( 
     <div className="App">
-      <ul>
-        <li>com</li>
-      </ul>
+        <ul>
+          <li>com</li>
+        </ul>
     </div>
   );
 }
