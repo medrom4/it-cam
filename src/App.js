01abe3f1750b15989c8043f597d5b094385a24d4
medrom4 <medikroma@gmail.com>
2019-09-02 16:18:11 +0300

    add-project

--- src/App.js
+++ src/App.js
@@ -2,17 +2,46 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import Header from './header.js';
-import Technologies from './technologies.js';
-import Footer from './footer';
-
 
 const App = () => {
   return (
-    <div>
-      <Header />
-      <Technologies />
-      <Footer />
+    <div className="app-wrapper">
+
+      <header className="header">
+        <img src="https://www.messengerpeople.com/wp-content/uploads/2019/01/icon-400-messenger-apple-messages-medium2x.png"></img>
+      </header>
+
+
+      <nav className="nav">
+        <div><a href="#s">Profile</a></div>
+        <div><a href="#s">Messages</a></div>
+        <div><a href="#s">News</a></div>
+        <div><a href="#s">Music</a></div><br />
+        <div><a href="#s">Settings</a></div>
+      </nav>
+
+
+      <div className="content">
+      <img src="http://www.unificatie.nl/wp-content/uploads/2015/03/flat_earth1.jpg"></img>
+      <div>
+        ava + description
+      </div>
+      <div>
+        my post
+        <div>
+          new post
+        </div>
+      </div>
+      <div>
+        <div>
+          post_1
+        </div>
+        <div>
+          post_2
+        </div>
+      </div>
+      </div>      
+
     </div>
   );
 }
